82294dfdc174aea2bd34c4b917c712bd35141e91
Anna Murphy <blm1003@rit.edu>
2019-05-26 06:33:21 -0400

    point_counter.handle() 2 arg conditions implemented
    the code for help, leaderboards, and user specific point data is
    implemented, but not tested.

--- point_counter.py
+++ point_counter.py
@@ -28,15 +28,42 @@ class Point_Counter:
             for name in data.keys():
                 f.write("{}:{}\n".format(name, str(data[name])))
 
+    def help (self):
+        return "I hear you're looking for help!\n" \
+                + "```!p tells you everyone's standing.\n" \
+                + "!p l tells you the top 3 people.\n" \
+                + "!p username gives the points for a specific user.\n" \
+                + "!p username X updates the score for that user.\n" \
+                + "```"
+
     def handle (self, msg):
         contents = msg.strip.split(" ")[1:]
         response = ""
         if len(contents) == 1:
-            # printing the score or help
+            # printing the score
             pass
         elif len(contents) == 2:
-            # either leaderboard or user score
-            pass
+            # either leaderboard, user score, or help
+            if contents[1] == "h":
+                #help message
+                response = self.help();
+            elif contens[1] == "l":
+                # leader board
+                leaders = []
+                counter = 1
+                for i in sorted (self.point_data):
+                    leaders.append(i)
+                for u in leaders:
+                    response.append("{}. {}\n".format(counter, u))
+                    counter += 1
+                    if counter == 4:
+                        break
+            elif contents[1] in self.point_data.keys():
+                # user score
+                response = "{} has {} points".format(contents[1], self.point_data[contents[1]])
+            else:
+                # error?
+                response = "The formatting of that doesn't feel quite right... try asking for help?"
         elif len(contnets) == 3:
             # update user score
             pass
