2312a871e242a18ddffc73c380bd87a5cc338e1e
Anna Murphy <blm1003@rit.edu>
2019-05-26 20:12:21 -0400

    test_point_counter - new
    Test file for point_counter class to make sure everything works as
    intended.

---
+++ test_point_counter.py
@@ -0,0 +1,18 @@
+import point_counter.py as p
+import unittest
+
+class TestPointCounter:
+
+    test_pc = None
+
+    def test_constructor(self):
+        pass
+
+    def test_str(self):
+        pass
+
+    def test_update(self):
+        pass
+
+    def test_handle (self):
+        pass
