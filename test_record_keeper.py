4401df407b1c5acfc3ad9cb9547de63ce5099696
Anna Murphy <blm1003@rit.edu>
2019-06-01 17:42:28 -0400

    test record keeper - test method stubs
    method stubs added for the record keeper and record test class

--- test_record_keeper.py
+++ test_record_keeper.py
@@ -3,7 +3,24 @@
     Author: Anna Murphy
 """
 
-class Test_Record_Keeper:
+import record_keeper as rk
+import unittest
 
-    def __init__(self):
+class Test_Record_Keeper (unittest.TestCase):
+
+    DIR_PATH = "records/"
+
+    def test_record_init (self):
+        pass
+
+    def test_record_contents (self):
+        pass
+
+    def test_keeper_init (self):
+        pass
+
+    def test_add_record (self):
+        pass
+
+    def test_keeper_getters (self):
         pass
