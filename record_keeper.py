7a26bfb5417fe707b2047e353552123e4053e8c4
Anna Murphy <blm1003@rit.edu>
2019-06-01 11:36:56 -0400

    record keeper and test - new
    New class and test class to manage the curation of puns

---
+++ record_keeper.py
@@ -0,0 +1,10 @@
+"""
+    File: record_keeper.py
+    Author: Anna Murphy
+"""
+
+class Record_Keeper:
+
+    def __init__ (self):
+        pass
+
---
+++ test_record_keeper.py
@@ -0,0 +1,9 @@
+"""
+    File: test_record_keeper.py
+    Author: Anna Murphy
+"""
+
+class Test_Record_Keeper:
+
+    def __init__(self):
+        pass
